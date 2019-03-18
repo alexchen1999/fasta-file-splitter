3cac30269f415fff69d39ca7ca4cd57f181719d3
Alex Chen <aecchen@sas.upenn.edu>
2019-03-18 18:01:50 -0400

    wrote ReadFileMethod

--- FastaFileSplitter/src/GenomeFileSplitter.java
+++ FastaFileSplitter/src/GenomeFileSplitter.java
@@ -1,4 +1,30 @@
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class GenomeFileSplitter {
-
+	
+	public String ReadFile(String filename) {
+		FileReader in;
+		try {
+			in = new FileReader(filename);
+			BufferedReader r = new BufferedReader(in);
+			Scanner scan = new Scanner(r);
+			String contents = "";
+			String line;
+			while ((line = scan.nextLine()) != null) {
+				contents += line + "\n";
+			}
+			scan.close();
+			return contents;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return null;	
+	}
+	
 }
